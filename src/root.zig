d280351e22eab870d6331c542af3577cfc2b8941
Benjamin Rabier <benjamin+git@rabier.dev>
2026-01-19 16:13:22 +0100

    better comment

--- src/root.zig
+++ src/root.zig
@@ -2,37 +2,40 @@
 const std = @import("std");
 const builtin = @import("builtin");
 
-const MXFP4_BLOCK_SIZE = 17;
+const MXFP4_BLOCK_BYTES_SIZE = 17;
 const MXFP4_VALUES_PER_BLOCK = 32;
 
 // https://www.opencompute.org/documents/ocp-microscaling-formats-mx-v1-0-spec-final-pdf
-const Mxfp4Adapter = struct {
+const Mxfp4Reader = struct {
     inner: std.io.Reader,
-    mxfp4_block: [MXFP4_BLOCK_SIZE]u8,
+    mxfp4_block: [MXFP4_BLOCK_BYTES_SIZE]u8,
+    f32_block: [MXFP4_VALUES_PER_BLOCK]f32,
     interface: std.io.Reader,
 
-    pub fn init(inner: std.io.Reader, buffer: []u8, comptime endianness: std.builtin.Endian) Mxfp4Adapter {
-        if (buffer.len < MXFP4_VALUES_PER_BLOCK * @sizeOf(f32)) @panic("Buffer must be at least 128 bits big to contain a single block.");
+    pub fn init(inner: std.io.Reader, buffer: []u8, comptime endianness: std.builtin.Endian) Mxfp4Reader {
+        if (buffer.len < MXFP4_VALUES_PER_BLOCK * @sizeOf(f32)) @panic("Buffer must be at least 128 bits big to contain all values from a single block.");
+        // We ensure with the parameter that the caller is aware of how we expose the f32 as bytes. Little-endian will be most likely what's needed
+        // and it's used by most modern arch, but better safe than sorry.
         if (endianness != builtin.target.cpu.arch.endian()) @panic("Only native endianness is supported by this adapter.");
 
         const reader = std.io.Reader{
-            .vtable = &.{ .stream = Mxfp4Adapter.stream },
+            .vtable = &.{ .stream = Mxfp4Reader.stream },
             .buffer = buffer,
             .seek = 0,
             .end = 0,
         };
-        return Mxfp4Adapter{ .inner = inner, .mxfp4_block = undefined, .interface = reader };
+        return Mxfp4Reader{ .inner = inner, .mxfp4_block = undefined, .f32_block = undefined, .interface = reader };
     }
 
     fn stream(r: *std.io.Reader, w: *std.io.Writer, limit: std.io.Limit) std.io.Reader.StreamError!usize {
-        const self: *Mxfp4Adapter = @fieldParentPtr("interface", r);
+        const self: *Mxfp4Reader = @fieldParentPtr("interface", r);
 
         try self.inner.readSliceAll(&self.mxfp4_block);
 
         const data: []const f32 = &([_]f32{1.0} ** MXFP4_VALUES_PER_BLOCK);
 
-        // Here I do assume that we want the f32 in the "native" endianness of the current architecture.
-        // Most modern architectures are little-endian, so
+        // Here we do assume that we want the current arch endianness which we checked in the init() function.
+        // Otherwise we would need to use the writeInt function when it differs which likely has some extra cost.
         const bytes: []const u8 = std.mem.sliceAsBytes(data);
 
         const n = limit.minInt(bytes.len);
@@ -68,7 +71,7 @@ test "Can read MXFP4" {
     defer gpa.free(buffer);
     std.debug.print("Loaded {d} bytes\n", .{test_case.mxfp4_bytes.len});
     const mxfp4_reader = std.io.Reader.fixed(test_case.mxfp4_bytes);
-    var reader = Mxfp4Adapter.init(mxfp4_reader, buffer, .little);
+    var reader = Mxfp4Reader.init(mxfp4_reader, buffer, .little);
 
     const result = try gpa.alignedAlloc(u8, .@"4", test_case.f32.len * 4);
     defer gpa.free(result);
